2c00feaef97f3b1bca48382fe899adf0208ef1f2
Tso4la1 <tsolajohn@gmail.com>
2023-12-09 23:10:23 +0100

    focus-area-text completed

--- src/Components/Organisms/InnovationConcept/index.jsx
+++ src/Components/Organisms/InnovationConcept/index.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { TextWrappers } from '../../Molecules/OpenningSpeech'
+import { Texts } from "../../Atoms/Texts";
+
+
 
 export const InnovationConcept = () => {
     return (
-        <div></div>
+        <TextWrappers
+            display="flex"
+            flex_direction="column"
+            justify_content="center"
+            align_items="center"
+
+        >
+            <TextWrappers
+                display="flex"
+                justify_content="center"
+                align_items="center"
+                font_size="50px"
+                width="60%"
+                className="innovation-concept
+    ">
+                <Texts name="paragraph" text="Our Concept Innovations" />
+            </TextWrappers>
+            <TextWrappers
+                display="flex"
+                flex_direction="column"
+                justify_content="center"
+                align_items="center"
+                width="100%"
+                className='innovation-concept-text'
+            >
+                <Texts name="paragraph" text='We have proprietary concept innovations that will support the startup ecosystem, support solution-providers/enterpreneurs and' />
+                <Texts name="paragraph" text='ultimately help democratize success;' />
+            </TextWrappers>
+        </TextWrappers>
+
     )
 }
